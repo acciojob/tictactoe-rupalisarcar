06eb143a646ba1cf5e9ad82cf6c23612291aac79
rupalisarcar <rupalisarcar@gmail.com>
2025-11-06 18:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ const start = document.getElementById("start");
 const firstPage = document.getElementById("firstPage");
 const gamePage = document.getElementById("gamePage");
 gamePage.style.display="none";
+let firstUser = true;
+	
 function startGame(){
-	const first_player = document.getElementById("first_player").value;
-	const sec_player = document.getElementById("sec_player").value;
+	const first_player = document.getElementById("player1").value;
+	const sec_player = document.getElementById("player2").value;
 	if(first_player!=="" && sec_player !==''){
 		firstPage.style.display="none";
 		gamePage.style.display="block";
-		const messageDiv = document.createElement('div')
-		messageDiv.classList.add('message')
-		p.textContent=`${first_player}, you're up`;		
+		const message_container = document.createElement('div');
+		message_container.classList.add('message')
+		message_container.textContent=`${first_player}, you're up`;		
 		gamePage.appendChild(p)
+		const game_container = document.createElement('div');
+		
 		
 	}
 }
