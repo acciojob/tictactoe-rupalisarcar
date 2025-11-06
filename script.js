1a64c9f91b6c0a151f3ca94cee4e264b91e54e6b
rupalisarcar <rupalisarcar@gmail.com>
2025-11-06 12:30:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const start = document.getElementById("start");
+const firstPage = document.getElementById("firstPage");
+const gamePage = document.getElementById("gamePage");
+gamePage.style.display="none";
+function startGame(){
+	const first_player = document.getElementById("first_player").value;
+	const sec_player = document.getElementById("sec_player").value;
+	if(first_player!=="" && sec_player !==''){
+		firstPage.style.display="none";
+		gamePage.style.display="block";
+		const p = document.createElement('p')
+		p.textContent=`${first_player}, you're up`;
+		
+		gamepage.appendChild(p)
+		
+	}
+}
+start.addEventListener("click", startGame)
